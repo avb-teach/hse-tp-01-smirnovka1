fcdebc70bd90dff2a70b79be47d66e29f6faf5b0
new name <bob@example.com>
2025-04-28 19:15:39 +0000

    final

--- collect_files.py
+++ collect_files.py
@@ -7,7 +7,6 @@ import argparse
 from pathlib import Path
 
 def copy_files(input_dir, output_dir, max_depth=None):
-
     input_path = Path(input_dir).resolve()
     output_path = Path(output_dir).resolve()
     
@@ -20,7 +19,7 @@ def copy_files(input_dir, output_dir, max_depth=None):
     else:
 
         copy_with_max_depth(input_path, output_path, max_depth)
-
+    
 
 def copy_without_structure(input_dir, output_dir):
 
@@ -37,38 +36,45 @@ def copy_with_max_depth(input_dir, output_dir, max_depth):
         root_path = Path(root)
         rel_path = root_path.relative_to(input_path)
         depth = len(rel_path.parts)
+        
+
         for filename in files:
             source_file = root_path / filename
             
             if depth <= max_depth:
 
-                target_dir = output_dir
-                if rel_path.parts:  
+                if rel_path.parts: 
                     target_dir = output_dir / rel_path
+                else:
+                    target_dir = output_dir
+                
                 target_dir.mkdir(parents=True, exist_ok=True)
                 copy_file_with_rename(source_file, target_dir)
             else:
 
-                start_index = depth - max_depth
-                
-                # Создаем целевой путь
-                new_path_parts = rel_path.parts[start_index:]
-                target_dir = output_dir
-                for part in new_path_parts[:-1]: 
-                    target_dir = target_dir / part
+                if depth - max_depth > 0:
+
+                    path_parts = rel_path.parts[max_depth-1:]  
+                    target_dir = output_dir
+                    
+                    for i, part in enumerate(path_parts[:-1]):
+                        target_dir = target_dir / part
+                        target_dir.mkdir(parents=True, exist_ok=True)
+                else:
+                    target_dir = output_dir / rel_path
                     target_dir.mkdir(parents=True, exist_ok=True)
                 
                 copy_file_with_rename(source_file, target_dir)
-
+                
 def copy_file_with_rename(src_file, dest_dir):
     filename = src_file.name
     dest_path = dest_dir / filename
-
+    
     if dest_path.exists():
         stem = src_file.stem
         suffix = src_file.suffix
         counter = 1
-
+        
         while (dest_dir / f"{stem}{counter}{suffix}").exists():
             counter += 1
         
@@ -83,8 +89,8 @@ def main():
     parser.add_argument("--max_depth", type=int)
     
     args = parser.parse_args()
+    
+    copy_files(args.input_dir, args.output_dir, args.max_depth)
 
 if __name__ == "__main__":
     main()
-
-    
